eee21e6936aa43f86bb014f0bc9475cc73b954dd
mikewanjero <mikewanj@gmail.com>
2025-01-23 14:18:23 +0300

    added subscription form

---
+++ src/components/Subscription/SubscriptionForm.jsx
@@ -0,0 +1,13 @@
+export default function SubscriptionForm() {
+  return (
+    <>
+      <div className="d-flex flex-column flex-lg-row w-100 h-100 p-4">
+        <form className="d-flex flex-column justify-content-center items-start w-100 lg-w-50 p-6 border-end h-100">
+          <h2 className="d-block text-start fs-4 medium mb-4 text-danger">
+            Activate My Subscription
+          </h2>
+        </form>
+      </div>
+    </>
+  );
+}
