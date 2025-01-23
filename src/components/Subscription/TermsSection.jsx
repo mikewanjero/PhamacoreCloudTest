14d4e0ed779bb06aa38a07799306be89c8dc908a
mikewanjero <mikewanj@gmail.com>
2025-01-23 14:18:36 +0300

    added terms section

---
+++ src/components/Subscription/TermsSection.jsx
@@ -0,0 +1,8 @@
+export default function TermsSection() {
+  return (
+    <div>
+      <h2>Terms and Conditions</h2>
+      <p>Read the terms and conditions before subscribing.</p>
+    </div>
+  );
+}
