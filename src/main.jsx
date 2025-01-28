6507917445b694b11d057fe3ec9d5508152e99fb
mikewanjero <mikewanj@gmail.com>
2025-01-28 15:13:15 +0300

    added browser router package for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 // import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./custom.scss";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
